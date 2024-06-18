0eb97bed14f284e61e8a74417733cf3e593a597c
kavinduUdhara <wmkavinduudhara@gmail.com>
2024-06-18 16:03:08 +0530

    intro activities done

---
+++ RandomWord.java
@@ -0,0 +1,23 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord {
+    public static void main(String[] args) {
+        String[] words = new String[0]; // Start with an empty array
+        String[] wordsSub = new String[0];
+
+        while (!StdIn.isEmpty()) {
+            String val = StdIn.readString();
+            wordsSub = new String[words.length + 1];
+            for (int i = 0; i < words.length; i++){
+                wordsSub[i] = words[i];
+            }
+            wordsSub[words.length] = val;
+            words = wordsSub;
+        }
+        int rand = StdRandom.uniformInt​(0, words.length);
+        StdOut.println(words[rand]);
+    }
+
+}
